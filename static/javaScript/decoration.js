260b4261bb54d6418e47d8f1224e69e42682f280
NKMAK <nakamuraakira18@gmail.com>
2023-08-06 17:40:48 +0900

    雪のパーティクル追加
    雪のパーティクル追加

--- static/javaScript/decoration.js
+++ static/javaScript/decoration.js
@@ -2,10 +2,12 @@ import * as THREE from 'three';
 
 const selectElement = document.getElementById("id_decorationSelect");
 let animationId;
-let particleGroup;
+let particleStarGroup;
+let particleSnowGroup1=new THREE.Group();;
+let particleSnowGroup2=new THREE.Group();;
 
 export function starCreate(scene){
-    particleGroup=createColorfulParticle(scene);
+    particleStarGroup=createColorfulParticle(scene);
 }
 
 
@@ -15,14 +17,14 @@ export function decorationSelectHandler(scene){
         switch(selectedIndex){
             case 1:
                 cancelAnimationFrame(animationId);
-                scene.remove(particleGroup);
-                particleGroup=createColorfulParticle(scene);
-                animate();
+                scene.remove(particleStarGroup);
+                particleStarGroup=createColorfulParticle(scene);
                 break;
             case 2:
                 cancelAnimationFrame(animationId);
-                scene.remove(particleGroup);
+                scene.remove(particleStarGroup);
                 createSnowParticle(scene);
+                animate();
                 break;
             case 3:
                 break;
@@ -34,7 +36,7 @@ export function decorationSelectHandler(scene){
 
 function createColorfulParticle(scene) {
     const group = new THREE.Group();
-    for (var i = 0; i < 3000; i++) {
+    for (let i = 0; i < 3000; i++) {
         const geometry = new THREE.BufferGeometry();
         const material = new THREE.PointsMaterial({ size: 5, color: Math.random() * 0xffffff });
   
@@ -55,35 +57,38 @@ function createColorfulParticle(scene) {
 }
 
 function createSnowParticle(scene) {
-    const group = new THREE.Group();
-    for (var i = 0; i < 3000; i++) {
+    for (let i = 0; i < 3000; i++) {
         const geometry = new THREE.BufferGeometry();
         const material = new THREE.PointsMaterial({ size: 5, color: 0xffffff });
   
         let positions = [];
         const particle = new THREE.Vector3();
         particle.x = Math.random() * 7000 - 3500;
-        particle.y = Math.random() * 2000 - 1000;
+        particle.y = Math.random() * 6000 - 3000;
         particle.z = Math.random() * 7000 - 3500;
   
         positions.push(particle.x, particle.y, particle.z);
         geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
   
         const particles = new THREE.Points(geometry, material);
-        group.add(particles);
+        (i < 1500) ?particleSnowGroup1.add(particles):particleSnowGroup2.add(particles);
     }
-    scene.add(group);
-    return group;
+    particleSnowGroup1.position.y=0;
+    particleSnowGroup2.position.y=3000;
+    scene.add(particleSnowGroup1);
+    scene.add(particleSnowGroup2);
 }
 
 function animate() {
     animationId=requestAnimationFrame(animate);
-
-
-    if (particleGroup.position.y < -1000) {
-      particleGroup.position.y = 0;
-    } else {
-      particleGroup.position.y -= 1;
-    }
+    particleSnowGroup1.position.y -= 5;
+    particleSnowGroup2.position.y -= 5;
+    
+    if (particleSnowGroup1.position.y < -3500) {
+        particleSnowGroup1.position.y = 4000;
+    } 
   
+    if (particleSnowGroup2.position.y < -3500) {
+        particleSnowGroup2.position.y = 4000;
+    } 
   }
