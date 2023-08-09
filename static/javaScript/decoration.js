a8985a960e46fa9ea9eb14efb9dcc6ade36eb3a4
NKMAK <nakamuraakira18@gmail.com>
2023-08-09 10:47:44 +0900

    decoration.jsにワイヤーフレームの図形追加
    decoration.jsにワイヤーフレームの図形追加

--- static/javaScript/decoration.js
+++ static/javaScript/decoration.js
@@ -3,11 +3,10 @@ import * as THREE from 'three';
 const selectElement = document.getElementById("id_decorationSelect");
 let animationId;
 let particleStarGroup;
-let particleSnowGroup1=new THREE.Group();
-let particleSnowGroup2=new THREE.Group();
-let particleRainGroup1=new THREE.Group();
-let particleRainGroup2=new THREE.Group();
-let createDonutGroup=new THREE.Group();
+let particleGroup1=new THREE.Group();
+let particleGroup2=new THREE.Group();
+let donutGroup=new THREE.Group();
+let randomShapeGroup=new THREE.Group();
 
 export function decorationSelectHandler(scene){
     selectElement.addEventListener("change", function() {
@@ -21,31 +20,37 @@ export function jsonLoadDecoration(selectedIndex,scene){
 }
 
 function loadDecoration(selectedIndex,scene){
-    cancelAnimationFrame(animationId);
-    scene.remove(particleStarGroup);
-    scene.remove(particleSnowGroup1);
-    scene.remove(particleSnowGroup2);
-    scene.remove(createDonutGroup);
     switch(selectedIndex){
         case 0:
+            cancelAnimationFrame(animationId);
+            scene.remove(particleStarGroup);
+            scene.remove(particleGroup1);
+            scene.remove(particleGroup2);
+            scene.remove(donutGroup);
             break;
         case 1:
             particleStarGroup=createColorfulParticle(scene);
             break;
+
         case 2:
             const snowParticleNum=3000;
             const snowSpeed=5;
             createDownParticle(scene,0xffffff,snowParticleNum,snowSpeed);
-
             break;
+
         case 3:
             const rainParticleNum=3000;
             const rainSpeed=5;
             createDownParticle(scene,0x00b3ff,rainParticleNum,rainSpeed);
             break;
+
         case 4:
             createDonut(scene);
             break;
+        case 5:
+            createRandomShape(scene);
+            break;
+
     }
 }
 
@@ -57,9 +62,9 @@ function createColorfulParticle(scene) {
   
         let positions = [];
         const particle = new THREE.Vector3();
-        particle.x = Math.random() * 7000 - 3500;
+        particle.x = Math.random() * 8000 - 4000;
         particle.y = Math.random() * 4000 - 2000;
-        particle.z = Math.random() * 7000 - 3500;
+        particle.z = Math.random() * 8000 - 4000;
   
         positions.push(particle.x, particle.y, particle.z);
         geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
@@ -72,28 +77,28 @@ function createColorfulParticle(scene) {
 }
 
 function createDownParticle(scene,color,particleNum,speed) {
-    particleSnowGroup1=new THREE.Group();
-    particleSnowGroup2=new THREE.Group();
+    particleGroup1=new THREE.Group();
+    particleGroup2=new THREE.Group();
     for (let i = 0; i < particleNum; i++) {
         const geometry = new THREE.BufferGeometry();
         const material = new THREE.PointsMaterial({ size: 5, color: color });
   
         let positions = [];
         const particle = new THREE.Vector3();
-        particle.x = Math.random() * 7000 - 3500;
+        particle.x = Math.random() * 8000 - 4000;
         particle.y = Math.random() * 6000 - 3000;
-        particle.z = Math.random() * 7000 - 3500;
+        particle.z = Math.random() * 8000 - 4000;
   
         positions.push(particle.x, particle.y, particle.z);
         geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
   
         const particles = new THREE.Points(geometry, material);
-        (i < 1500) ?particleSnowGroup1.add(particles):particleSnowGroup2.add(particles);
+        (i < 1500) ?particleGroup1.add(particles):particleGroup2.add(particles);
     }
-    particleSnowGroup1.position.y=0;
-    particleSnowGroup2.position.y=3000;
-    scene.add(particleSnowGroup1);
-    scene.add(particleSnowGroup2);
+    particleGroup1.position.y=0;
+    particleGroup2.position.y=3000;
+    scene.add(particleGroup1);
+    scene.add(particleGroup2);
     animate(speed);
 }
 
@@ -104,22 +109,62 @@ function createDonut(scene){
 
         const donut=new THREE.Mesh(donutGeometry, DonutMesh)
         donut.position.set(0, 0, i*(-800));      
-        createDonutGroup.add(donut)
+        donutGroup.add(donut)
     }
-    scene.add( createDonutGroup);
+    scene.add( donutGroup);
+}
+
+function createRandomShape(scene) {
+    let geometry;
+    let shape;
+    for(let i=0; i<300; i++){
+        const geometryType = Math.floor(Math.random() * 4);
+        const  material=new THREE.MeshBasicMaterial({ color: Math.random() *0xffffff, wireframe: true});
+        switch (geometryType) {
+        case 0: 
+            geometry = new THREE.ConeGeometry(20, 20, 3);
+            shape = new THREE.Mesh(geometry, material);
+            break;
+
+        case 1: 
+            geometry = new THREE.SphereGeometry(25, 5, 5);
+            shape = new THREE.Mesh(geometry,material);
+            break;
+
+        case 2: 
+            geometry = new THREE.BoxGeometry(20, 20, 20);
+            shape = new THREE.Mesh(geometry, material);
+            break;
+
+        case 3: 
+            geometry = new THREE.CylinderGeometry(20,20, 30, 10);
+            shape = new THREE.Mesh(geometry,material);
+            break;
+        }
+        shape.position.x = Math.random() * 8000 - 4000;
+        shape.position.y = Math.random() * 4000 - 2000;
+        shape.position.z = Math.random() * 8000 - 4000;
+
+        shape.rotation.x = Math.random() * Math.PI * 2;
+        shape.rotation.y = Math.random() * Math.PI * 2;
+        shape.rotation.z = Math.random() * Math.PI * 2;
+        
+        randomShapeGroup.add(shape);
+    }
+    scene.add(randomShapeGroup);
 }
 
 function animate(speed) {
     animationId=requestAnimationFrame(animate);
-    particleSnowGroup1.position.y -= 5;
-    particleSnowGroup2.position.y -= 5;
+    particleGroup1.position.y -= 5;
+    particleGroup2.position.y -= 5;
     
-    if (particleSnowGroup1.position.y < -3500) {
-        particleSnowGroup1.position.y = 4000;
+    if (particleGroup1.position.y < -3500) {
+        particleGroup1.position.y = 4000;
     } 
   
-    if (particleSnowGroup2.position.y < -3500) {
-        particleSnowGroup2.position.y = 4000;
+    if (particleGroup2.position.y < -3500) {
+        particleGroup2.position.y = 4000;
     } 
 }
 
